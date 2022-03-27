340b08c67d7499d3224b5bcc2ebcbf9157ff4e4a
aaronmsimon <aaronsimon@gmail.com>
2022-03-26 19:34:23 -0700

    New Team menu
    add Home and Schedule

--- application/controllers/Teams.php
+++ application/controllers/Teams.php
@@ -10,10 +10,21 @@ class Teams extends MY_Controller {
 		$this->load->model('teams_model');
 	}
 
-  public function roster($teamabbr) {
-    $this->data['team'] = $this->teams_model->getTeamByAbbr($teamabbr);
+	public function home($teamabbr) {
+		$this->data['team'] = $this->teams_model->getTeamByAbbr($teamabbr);
+		$this->load->view('teamhome',$this->data);
+	}
+
+	public function roster($teamabbr) {
+		$this->data['team'] = $this->teams_model->getTeamByAbbr($teamabbr);
 		$this->data['roster'] = $this->teams_model->getRosterByTeamID($this->data['team']->id);
-    $this->load->view('roster',$this->data);
-  }
+		$this->load->view('roster',$this->data);
+	}
+
+	public function schedule($teamabbr) {
+		$this->data['team'] = $this->teams_model->getTeamByAbbr($teamabbr);
+		$this->data['results'] = $this->teams_model->getResultsByTeamID($this->data['team']->id);
+		$this->load->view('teamschedule',$this->data);
+	}
 
 }
